1e5775775f05681154ef84a3c7cef7f2a58720e6
sabujAlom <sabujalom18@gmail.com>
2026-07-23 21:09:41 +0600

    JWKS added

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -3,7 +3,9 @@
 import BookingCard from "@/components/BookingCard";
 import { DeleteAlert } from "@/components/DeleteAlert";
 import { EditModal } from "@/components/EditModal";
+import { auth } from "@/lib/auth";
 import { Button } from "@heroui/react";
+import { headers } from "next/headers";
 import Image from "next/image";
 import { BiEdit } from "react-icons/bi";
 import { FaRegCalendar } from "react-icons/fa";
@@ -12,8 +14,18 @@ import { LuMapPin } from "react-icons/lu";
 
 
 const DestinationDetailsPage =async ({params}) => {
-    const {id} = await params
-    const res = await fetch(`http://localhost:5000/destination/${id}`)
+    const {id} = await params;
+    const {token} =await auth.api.getToken({
+      headers: await headers()
+
+    })
+     console.log(token)
+
+    const res = await fetch(`http://localhost:5000/destination/${id}`, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    });
     const destination = await res.json()
      const {imageUrl, price, destinationName, duration, country, description} = destination;
 
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,8 @@
-import { betterAuth } from "better-auth";
+import { betterAuth} from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("wanderlust");
@@ -18,4 +20,15 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         },
       },
+      session:{
+        cookieCache:{
+          enabled: true,
+          strategy: "jwt",
+          //max: 7days
+          maxAge: 7 * 24 * 60 * 60
+        }
+      },
+      plugins:[
+        jwt()
+      ]
 });
